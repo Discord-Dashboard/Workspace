d36a4083e530f9e7973de6c3e2168db99932d8b9
Marcin Kondrat <breftejk@gmail.com>
2024-11-25 00:07:48 +0100

    feat(discord-dashboard.config.js): add logging configuration to enhance debugging capabilities

--- packages/core/test/discord-dashboard.config.js
+++ packages/core/test/discord-dashboard.config.js
@@ -1,5 +1,12 @@
+// config.js
+/** @type {import('./IConfig').IConfig} */
 module.exports = {
   server: {
     port: 3000,
+  },
+  logs: {
+    saveToFile: true,
+    file: 'discord-dashboard.log',
+    level: 'DEVELOPMENT',
   },
 };
