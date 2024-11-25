6fa373958c32e6e3454cfc1be769f6fdba622876
Marcin Kondrat <breftejk@gmail.com>
2024-11-25 16:02:26 +0100

    feat(jest.config.ts): enable coverage collection and specify coverage reporters to enhance test reporting and analysis

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,4 +1,7 @@
 export default {
+  collectCoverage: true,
+  coverageReporters: ['cobertura', 'lcov', 'text'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
   displayName: 'core',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
